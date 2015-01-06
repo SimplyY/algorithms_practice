602a1f77316b1a34b1d458b764dec2534808493c
SimplyY <SimplyYu@163.com>
2015-01-06 20:47:36 +0800

    完成了归并排序的后两种

--- 第2章_mergeSorted/src/com/yuwei/Array.java
+++ 第2章_mergeSorted/src/com/yuwei/Array.java
@@ -2,7 +2,8 @@
  * Created by yuwei on 2015/1/5.
  */
 package com.yuwei;
-import java.math.*;
+
+
 public class Array {
     private int begin;
     private int end;
@@ -13,7 +14,7 @@ public class Array {
     private int c;
     private int mergeWay;
 
-    Array(int[] array){
+    Array(int[] array) {
         begin = 0;
         end = array.length - 1;
         this.array = array;
@@ -21,50 +22,65 @@ public class Array {
         c = 0;
     }
 
-    public void doMergeSort(int mergeWay){
+    public void doMergeSort(int mergeWay) {
         this.mergeWay = mergeWay;
-        mergeSort(begin, end, array);
+        if(mergeWay!=3){
+            mergeSort(begin, end, array);
+        }
+        else{
+            specialMergeSort(begin, end, array);
+        }
         showMergeSort();
     }
-    private void showMergeSort(){
+
+    private void showMergeSort() {
         System.out.println("排序方式：" + mergeWay + "\n排序完成后数组：");
 
         for (int i = 0; i < array.length; i++) {
             System.out.print("" + array[i] + " ");
         }
-        double lgn = Math.log(array.length)/Math.log(2);
-        c = (int)(times / (array.length * lgn));
+        double lgn = Math.log(array.length) / Math.log(2);
+        c = (int) (times / (array.length * lgn));
 
 
         System.out.println("\n运算次数：" + times + "  算法复杂度为nlgn，系数c不大于:" + c);
     }
 
 
-    private void mergeSort(int begin, int end, int[] array){
-
-        if(begin < end ){
-            int dividePosition = (begin + end)/2;
+    private void mergeSort(int begin, int end, int[] array) {
+        if (begin < end) {
+            int dividePosition = (begin + end) / 2;
 
             mergeSort(begin, dividePosition, array);
             mergeSort(dividePosition + 1, end, array);
-            if (mergeWay == 1){
-                merge1(begin, dividePosition, end,array);
-            }
-            else if(mergeWay == 2){
-                merge2(begin, dividePosition, end,array);
-            }
-            else if(mergeWay == 3){
-                merge3(begin, dividePosition, end,array);
+            if (mergeWay == 1) {
+                merge1(begin, dividePosition, end, array);
+            } else if (mergeWay == 2) {
+                merge2(begin, dividePosition, end, array);
             }
             times += 5;
         }
         times++;
 
+    }
 
+    //mergeWay == 3,用哨兵， 小数组内使用插入排序
+    private void specialMergeSort(int begin, int end, int[] array) {
+        if (begin < end - 2) {
+            int dividePosition = (begin + end) / 2;
+
+            specialMergeSort(begin, dividePosition, array);
+            specialMergeSort(dividePosition + 1, end, array);
+            insertSort(begin, dividePosition, array);
+            insertSort(dividePosition + 1, end, array);
+            merge1(begin, dividePosition, end, array);
+            times += 5;
+        }
+        times++;
     }
 
     //用哨兵，当使用哨兵的时候，会使array1和array2的长度加1！！！
-    private void merge1(int begin, int dividePosition, int end,int[] array){
+    private void merge1(int begin, int dividePosition, int end, int[] array) {
         int[] array1 = new int[dividePosition - begin + 2];
         int[] array2 = new int[end - dividePosition + 1];
         array1[array1.length - 1] = Integer.MAX_VALUE;
@@ -80,13 +96,12 @@ public class Array {
             times += 2;
         }
 
-        for (int i = begin, index1 = 0,index2 = 0; i <= end; i++) {
-            if(array1[index1] < array2[index2]){
+        for (int i = begin, index1 = 0, index2 = 0; i <= end; i++) {
+            if (array1[index1] < array2[index2]) {
                 array[i] = array1[index1];
                 index1++;
                 times += 4;
-            }
-            else{
+            } else {
                 array[i] = array2[index2];
                 index2++;
                 times += 4;
@@ -95,34 +110,59 @@ public class Array {
     }
 
     //不用哨兵
-    private void merge2(int begin, int dividePosition, int end, int[] array){
+    private void merge2(int begin, int dividePosition, int end, int[] array) {
         int[] array1 = new int[dividePosition - begin + 1];
         int[] array2 = new int[end - dividePosition];
+        times += 4;
 
         for (int i = 0; i < array1.length; i++) {
-            array1[i] = array[i];
+            array1[i] = array[begin + i];
+            times += 2;
         }
         for (int i = 0; i < array2.length; i++) {
-            array2[i] = array[array1.length + i];
+            array2[i] = array[dividePosition + i + 1];
+            times += 2;
         }
 
-        for (int i = begin,index1 = 0,index2 = 0; i < end; i++) {
-            if(array1[index1] < array2[index2]){
+        for (int i = begin, index1 = 0, index2 = 0; i <= end; i++) {
+            if (index1 < array1.length && index2 < array2.length) {
+                if (array1[index1] < array2[index2]) {
+                    array[i] = array1[index1];
+                    index1++;
+                    times += 4;
+                } else {
+                    array[i] = array2[index2];
+                    index2++;
+                    times += 4;
+                }
+            } else if (index1 < array1.length) {
                 array[i] = array1[index1];
                 index1++;
-            }
-            else{
+                times += 4;
+            } else if (index2 < array2.length) {
                 array[i] = array2[index2];
                 index2++;
+                times += 5;
             }
         }
     }
 
-    //对小数组采用插入排序
-    private void merge3(int begin, int dividePosition, int end, int[] array){
 
+    private void insertSort(int begin, int end, int[] array){
+        for (int i = begin + 1; i < end; i++) {
+            int key = array[i];
+            times += 2;
+            for (int j = i - 1; j > 0; j--) {
+                if(key < array[j]){
+                    array[j + 1] = array[j];
+                    times += 3;
+                }
+                else{
+                    array[j + 1] = key;
+                    times +=3;
+                    break;
+                }
+            }
+        }
     }
-
-
-
 }
--- 第2章_mergeSorted/src/com/yuwei/Main.java
+++ 第2章_mergeSorted/src/com/yuwei/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] sortedArray = new int[]{0,1,2,5,12,12,45,78,84,111,114,145,202,205,510,610};
+        int[] sortedArray = new int[]{3,41,52,26,38,57,9,49};
         int mergeWay1 = 1;
         int mergeWay2 = 2;
         int mergeWay3 = 3;
@@ -17,11 +17,11 @@ public class Main {
         Array array1 = new Array(sortedArray);
         array1.doMergeSort(mergeWay1);
 
-//        Array array2 = new Array(sortedArray);
-//        array1.doMergeSort(mergeWay1);
-//
-//        Array array3 = new Array(sortedArray);
-//        array1.doMergeSort(mergeWay1);
+        Array array2 = new Array(sortedArray);
+        array2.doMergeSort(mergeWay2);
+
+        Array array3 = new Array(sortedArray);
+        array3.doMergeSort(mergeWay3);
 
 
     }
